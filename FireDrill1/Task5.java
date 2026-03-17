f2697b918c73e7fa86c2bf09ae012d046111cf44
Ericsson Raphael <ericssonraphael@gmail.com>
2026-03-17 12:36:22 +0100

    Update: Task5

--- FireDrill1/Task5.java
+++ FireDrill1/Task5.java
@@ -1,9 +1,9 @@
-public class Task4{
+public class Task5{
 	public static void main(String[] args){
 		
 		int i = 0;
 		while(i++ < 100) {
-			if(i % 4 == 0) System.out.printf("%d ", i);
+			if(i % 4 == 0) System.out.printf("%d%d%d%d ", i, i, i, i);
 		}
 	} 
 
