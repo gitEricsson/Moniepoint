86d328ca2954cb9429fda35f03054dc156943fe3
gitEricsson <ericssonraphael@gmail.com>
2026-03-17 04:50:09 +0100

    Update: ArithmeticSmallestAndLargest️

--- Java/ArithmeticSmallestAndLargest.java
+++ Java/ArithmeticSmallestAndLargest.java
@@ -8,14 +8,14 @@ public class ArithmeticSmallestAndLargest {
         int number = input.nextInt();
         int numberSquared = number * number;
 
-        if (number > 100) System.out.printf("Is %d > 100? %n", number);
-        if (number == 100) System.out.printf("Is %d == 100? %n", number);
-        if (number != 100) System.out.printf("Is %d != 100? %n", number);
-        if (number < 100) System.out.printf("Is %d < 100? %n", number);
+        if (number > 100) System.out.printf("%d is greater than 100%n", number);
+        if (number == 100) System.out.printf("%d is equal to 100%n", number);
+        if (number != 100) System.out.printf("%d is not equal to 100%n", number);
+        if (number < 100) System.out.printf("%d is less than 100%n", number);
 
-        if (numberSquared > 100) System.out.printf("Is %d > 100? %n", numberSquared);
-        if (numberSquared == 100) System.out.printf("Is %d == 100? %n", numberSquared);
-        if (numberSquared != 100) System.out.printf("Is %d != 100? %n", numberSquared);
-        if (numberSquared < 100) System.out.printf("Is %d < 100? %n", numberSquared);
+        if (numberSquared > 100) System.out.printf("%d is greater than 100%n", numberSquared);
+        if (numberSquared == 100) System.out.printf("%d is equal to 100%n", numberSquared);
+        if (numberSquared != 100) System.out.printf("%d is not equal to 100%n", numberSquared);
+        if (numberSquared < 100) System.out.printf("%d is less than 100%n", numberSquared);
     }
 }
