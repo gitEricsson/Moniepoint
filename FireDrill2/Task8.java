11d1b004eb773484739aa944525f5bb6dba5618d
Ericsson Raphael <ericssonraphael@gmail.com>
2026-03-17 16:09:48 +0100

    Update/Fix: Task 9, Task 8

--- FireDrill2/Task8.java
+++ FireDrill2/Task8.java
@@ -8,44 +8,56 @@ public class Task8 {
 
         System.out.print("Enter first integer: ");
         int firstNumber = input.nextInt();
-        if(firstNumber >= 0 && firstNumber <= 100) sumOfEvenNumbers += firstNumber;
+
+        while(firstNumber <= 0 || firstNumber >= 100) firstNumber = input.nextInt();
+	    sumOfEvenNumbers += firstNumber;
+
 
         System.out.print("Enter second integer: ");
         int secondNumber = input.nextInt();
-        if(secondNumber >= 0 && secondNumber <= 100) sumOfEvenNumbers += secondNumber;
+        while(secondNumber <= 0 || secondNumber >= 100) secondNumber = input.nextInt();
+        sumOfEvenNumbers += secondNumber;
 
         System.out.print("Enter third integer: ");
         int thirdNumber = input.nextInt();
-        if(thirdNumber >= 0 && thirdNumber <= 100) sumOfEvenNumbers += thirdNumber;
+        while(thirdNumber <= 0 || thirdNumber >= 100) thirdNumber = input.nextInt();
+        sumOfEvenNumbers += thirdNumber;
 
         System.out.print("Enter fourth integer: ");
         int fourthNumber = input.nextInt();
-        if(fourthNumber >= 0 && fourthNumber <= 100) sumOfEvenNumbers += fourthNumber;
+        while(fourthNumber <= 0 || fourthNumber >= 100) fourthNumber = input.nextInt();
+        sumOfEvenNumbers += fourthNumber;
 
         System.out.print("Enter fifth integer: ");
         int fifthNumber = input.nextInt();
-        if(fifthNumber >= 0 && fifthNumber <= 100) sumOfEvenNumbers += fifthNumber;
+        while(fifthNumber <= 0 || fifthNumber >= 100) fifthNumber = input.nextInt();
+        sumOfEvenNumbers += fifthNumber;
 
         System.out.print("Enter sixth integer: ");
         int sixthNumber = input.nextInt();
-        if(sixthNumber >= 0 && sixthNumber <= 100) sumOfEvenNumbers += sixthNumber;
+        while(sixthNumber <= 0 || sixthNumber >= 100) sixthNumber = input.nextInt();
+        sumOfEvenNumbers += sixthNumber;
 
         System.out.print("Enter seventh integer: ");
         int seventhNumber = input.nextInt();
-        if(seventhNumber >= 0 && seventhNumber <= 100) sumOfEvenNumbers += seventhNumber;
+        while(seventhNumber <= 0 || seventhNumber >= 100) seventhNumber = input.nextInt();
+        sumOfEvenNumbers += seventhNumber;
 
         System.out.print("Enter eighth integer: ");
         int eighthNumber = input.nextInt();
-        if(eighthNumber >= 0 && eighthNumber <= 100) sumOfEvenNumbers += eighthNumber;
+        while(eighthNumber <= 0 || eighthNumber >= 100) eighthNumber = input.nextInt();
+        sumOfEvenNumbers += eighthNumber;
 
         System.out.print("Enter ninth integer: ");
         int ninthNumber = input.nextInt();
-        if(ninthNumber >= 0 && ninthNumber <= 100) sumOfEvenNumbers += ninthNumber;
+        while(ninthNumber <= 0 || ninthNumber >= 100) ninthNumber = input.nextInt();
+        sumOfEvenNumbers += ninthNumber;
 
         System.out.print("Enter tenth integer: ");
         int tenthNumber = input.nextInt();
-        if(tenthNumber >= 0 && tenthNumber <= 100) sumOfEvenNumbers += tenthNumber;
+        while(tenthNumber <= 0 || tenthNumber >= 100) tenthNumber = input.nextInt();
+        sumOfEvenNumbers += tenthNumber;
 
-        System.out.printf("The Sum of the Valid numbers between 0 and 100 is %d%n", sumOfEvenNumbers);
+        System.out.printf("The Sum of the 10 integers between 0 and 100 is %d%n", sumOfEvenNumbers);
     }
 }
