dd936f9c17837ec3a8cb60deddc0660e617e336b
Ericsson Raphael <ericssonraphael@gmail.com>
2026-03-21 19:39:38 +0100

    Feat: TDD style - all test cases for functions class

--- test/Snacks/FunctionsTest.java
+++ test/Snacks/FunctionsTest.java
@@ -30,4 +30,60 @@ public class FunctionsTest {
         assertThrows(IllegalArgumentException.class, () -> {functions.isEven(3.4);
         });
     }
+
+    @Test
+    @DisplayName("Test isPrimeNumber Function Operation")
+    public void testIsPrimeNumberFunction() {
+        assertTrue(functions.isPrimeNumber(7));
+        assertFalse(functions.isPrimeNumber(4));
+    }
+
+    @Test
+    @DisplayName("Test subtract Function Operation")
+    public void testSubtractFunction() {
+        assertEquals(4, functions.subtract(3, 7));
+        assertEquals(4, functions.subtract(7, 3));
+    }
+
+    @Test
+    @DisplayName("Test divide Function Operation")
+    public void testDivideFunction() {
+        assertEquals(2.5, functions.divide(5, 2));
+        assertEquals(0, functions.divide(5, 0));
+    }
+
+    @Test
+    @DisplayName("Test factorOf Function Operation")
+    public void testFactorOfFunction() {
+        assertEquals(4, functions.factorOf(10));
+        assertEquals(2, functions.factorOf(7));
+    }
+
+    @Test
+    @DisplayName("Test isSquare Function Operation")
+    public void testIsSquareFunction() {
+        assertTrue(functions.isSquare(25));
+        assertFalse(functions.isSquare(26));
+    }
+
+    @Test
+    @DisplayName("Test isPalindrome Function Operation")
+    public void testIsPalindromeFunction() {
+        assertTrue(functions.isPalindrome(54145));
+        assertFalse(functions.isPalindrome(12345));
+    }
+
+    @Test
+    @DisplayName("Test factorialOf Function Operation")
+    public void testFactorialOfFunction() {
+        assertEquals(120, functions.factorialOf(5));
+        assertEquals(1, functions.factorialOf(0));
+    }
+
+    @Test
+    @DisplayName("Test squareOf Function Operation")
+    public void testSquareOfFunction() {
+        assertEquals(25, functions.squareOf(5));
+        assertEquals(100, functions.squareOf(-10));
+    }
 }
