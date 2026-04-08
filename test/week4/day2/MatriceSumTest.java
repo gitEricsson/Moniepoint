14c53de28c4b087c7dc6133e607433a77674d6d9
Ericsson Raphael <ericssonraphael@gmail.com>
2026-04-08 11:24:29 +0100

    DSA: Matrix Sum

--- test/week4/day2/MatriceSumTest.java
+++ test/week4/day2/MatriceSumTest.java
@@ -3,11 +3,12 @@ package week4.day2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MatriceSumTest {
     @Test
-    @DisplayName("Test should return equal sum across rows, columns and diagonals")
+    @DisplayName("Test should return true for equal sum across rows, columns and diagonals")
     public void testEquivalentSumAcross3x3RowsColumnsAndDiagonals() {
         int[][] matrix = {
                 {8, 1, 6},
@@ -19,7 +20,7 @@ public class MatriceSumTest {
     }
 
     @Test
-    @DisplayName("Test should return equal sum across rows, columns and diagonals")
+    @DisplayName("Test should return true for equal sum across rows, columns and diagonals")
     public void testEquivalentSumAcross4x4RowsColumnsAndDiagonals() {
         int[][] matrix = {
                 {16,   2,   3,  13},
@@ -30,5 +31,19 @@ public class MatriceSumTest {
         };
 
         assertTrue(MatriceSum.sum(matrix));
+    }
+
+    @Test
+    @DisplayName("Test should return false for unequal sum across rows, columns and diagonals")
+    public void testFalseSumAcross4x4RowsColumnsAndDiagonals() {
+        int[][] matrix = {
+                {13,   2,   3,  13},
+                {5,  11,  10,   8},
+                {9,   7,   6,  12},
+                {4,  14,  15,   1}
+
+        };
+
+        assertFalse(MatriceSum.sum(matrix));
     }
 }
